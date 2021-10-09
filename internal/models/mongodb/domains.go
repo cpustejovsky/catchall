691ed85bf30b3ecf48e4166ddcdbe5b55c742cb6
cpustejovsky <charles.pustejovsky@gmail.com>
2021-10-09 11:09:21 -0400

    Update Domain Model to update delivered, bounced, and to check status

--- internal/models/mongodb/domains.go
+++ internal/models/mongodb/domains.go
@@ -1,9 +1,14 @@
 package mongodb
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type DomainModel struct {
@@ -12,11 +17,62 @@ type DomainModel struct {
 
 func (d *DomainModel) UpdateDelivered(name string) error {
 	log.Println(name)
+	_, err := d.DB.UpdateOne(context.TODO(), bson.M{
+		"name": name,
+	}, bson.D{
+		{"$inc", bson.D{{"delivered", 1}}},
+	}, options.Update().SetUpsert(true))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 	return nil
 }
 func (d *DomainModel) UpdateBounced(name string) error {
+	log.Println(name)
+	_, err := d.DB.UpdateOne(context.TODO(), bson.M{
+		"name": name,
+	}, bson.D{
+		{"$inc", bson.D{{"bounced", 1}}},
+	}, options.Update().SetUpsert(true))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 	return nil
 }
 func (d *DomainModel) CheckStatus(name string) (string, error) {
+	domain := bson.M{}
+	err := d.DB.FindOne(context.TODO(), bson.M{
+		"name": name,
+	}).Decode(&domain)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	bounced := domain["bounced"]
+	delivered := domain["delivered"]
+	fmt.Printf("bounced = %T\n", bounced)
+	fmt.Printf("delivered = %T\n", delivered)
+	bouncedInt, ok := bounced.(int32)
+	if !ok {
+		error := errors.New("Bounced did not convert to integer")
+		log.Println(error)
+		return "", error
+	}
+	deliveredInt, ok := delivered.(int32)
+	if !ok {
+		error := errors.New("Delivered did not convert to integer")
+		log.Println(error)
+		return "", error
+	}
+	if bouncedInt >= 1 {
+		return "not a catch-all", nil
+	}
+	if deliveredInt >= 1000 {
+		return "catch-all", nil
+	} else {
+		return "unknown", nil
+	}
 	return "foo", nil
 }
