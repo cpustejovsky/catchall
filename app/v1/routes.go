046de3c9f4e28ee7e299bb4f3f960cdac1373602
cpustejovsky <charles.pustejovsky@gmail.com>
2021-10-12 22:02:19 -0400

    Revert "Remove middleware from mux"
    This reverts commit 82e72b062cb662c0b713144dcd457fc58dd74a9c.

--- app/v1/routes.go
+++ app/v1/routes.go
@@ -4,15 +4,18 @@ import (
 	"net/http"
 
 	"github.com/bmizerany/pat"
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := pat.New()
 
-	mux.Put("/events/:domain_name/delivered", http.HandlerFunc(app.updateDelivered))
-	mux.Put("/events/:domain_name/bounced", http.HandlerFunc(app.updateBounced))
-	mux.Get("/domains/:domain_name", http.HandlerFunc(app.checkStatus))
-	mux.Get("/ping", http.HandlerFunc(app.ping))
+	mux.Put("/events/:domain_name/delivered", standardMiddleware.ThenFunc(app.updateDelivered))
+	mux.Put("/events/:domain_name/bounced", standardMiddleware.ThenFunc(app.updateBounced))
+	mux.Get("/domains/:domain_name", standardMiddleware.ThenFunc(app.checkStatus))
+	mux.Get("/ping", standardMiddleware.ThenFunc(app.ping))
 
-	return mux
+	return standardMiddleware.Then(mux)
 }
